4c210b4ba8425592813d7b495745d1658063e9e2
Matthew Miskov <mattm479@gmail.com>
2024-08-16 16:57:58 -0400

    mobile fixes

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,10 +1,9 @@
-import React from 'react';
 import './style.css';
 
 function Header() {
   return (
     <header className="header">
-    <div className="header-logo">The Book Nook</div>
+    <div className="header-logo"></div>
     <div className="header-search">
         <input
           type="text"
